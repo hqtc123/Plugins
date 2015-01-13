e6df3e29e320f53ade1552947430fe3455ee499a
hqtc123 <heqingnju@foxmail.com>
2015-01-13 16:48:45 +0800

    alarm success

--- alert2floor/detect.js
+++ alert2floor/detect.js
@@ -1,18 +1,50 @@
-var winid = 0;
-var tabid = 0;
-chrome.extension.onRequest.addListener(
-               function(request,sender,sendResponse){ 
-                      chrome.pageAction.show(sender.tab.id);
-                      chrome.windows.getCurrent(function(Win){ 
-                               winid = Win.id
-                      });
-                      circle();
+function search() {
+    return Array.prototype.map.call(
+        document.getElementsByClassName("j_thread_list"),
+        function(feed){
+            var reply = feed.getElementsByClassName("threadlist_rep_num")[0];
+            var replyNum = reply.textContent;
+            if(replyNum < 1){
+                var title = feed.getElementsByClassName("j_th_tit")[0].textContent
+                var author_id = feed.getElementsByClassName("j_user_card")[0].textContent
+
+                return {
+                    title: title,
+                    author_id: author_id
+//                     position: position(feed)
+                }; //sofa：沙发名，lz：楼主名，position：状态在页面中的绝对坐标
             }
-);
-function circle()
-{
-      radomURL = "http://tieba.baidu.com/f?kw=%C4%CF%BE%A9%B4%F3%D1%A7&fr=index"; //随机生成论坛网页代码，需要根据自己实际情况修改
-      chrome.tabs.create({windowId:winid,url: radomURL,selected:false},function(tab){tabid = tab.id}); 
-      chrome.tabs.remove(tabid); 
-      setTimeout(circle,60000); 
+        }).filter(function(n){return !!n;});
 }
+
+//function position(feed) {
+//    var x = feed.offsetLeft,
+//        y = feed.offsetTop
+//            while (feed = feed.offsetParent) {
+//                x += feed.offsetLeft
+//                    y += feed.offsetTop
+//            }
+//    return {
+//        x: x,
+//        y: y
+//    }
+//}
+//
+////----------------main----------------------
+console.log("监听器启动")
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.message) {
+    case "sofa":
+        sendResponse({
+            result: search()
+        });
+        break;
+    default:
+        sendResponse({
+            result: "不告诉你"
+        });
+    }
+})
+
+//-----------------全局变量-------------------
+var paddingTop = 20 //状态的CSS里找到的属性
--- alert2floor/popup.js
+++ alert2floor/popup.js
@@ -1,12 +1,37 @@
-$(function(){
+function init(){
+    contentDiv = document.createElement("div")
+    contentDiv.id = "contentDiv"
+    document.body.appendChild(contentDiv)
+    noResultDiv = document.createElement("div")
+    noResultDiv.textContent = "暂未检测到新贴"
+    contentDiv.appendChild(noResultDiv)
+}
+
+function go(){
   chrome.tabs.query({
     active:true,
     currentWindow:true
   },function(tabs){
     chrome.tabs.sendMessage(tabs[0].id,{
-      message:"sofa"
-    },function(response){
+      message: "sofa"
+    }, function(response){
+      for(var i = 0, pair; pair = response.result[i]; i++){
+        var p_ele = document.createElement("p");
+        var title = pair.title;
+        var author_id = pair.author_id;
 
+        noResultDiv.style.display = "none"
+        p_ele.innerHTML = author_id +" 刚发了一贴 "+title+";</br> 快去抢个2楼 !";
+        contentDiv.appendChild(p_ele);
+      }
     })
   })
-})
+}
+
+
+var contentDiv //内容块
+var noResultDiv
+
+//----------------main----------------------
+init()
+go()
